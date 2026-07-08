7806d537518d3c3b63d1f04070ed406de6c229bc
“Calixto” <l19070031@huetamo.tecnm.mx>
2026-07-07 20:56:09 -0600

    Agregando rutas en etiquetas de redes sociales

--- public/js/functions/functions.js
+++ public/js/functions/functions.js
@@ -1,13 +1,26 @@
 export function showPassword(input, inputConfirm, visible) {
-    if (input.type === 'password') {
-        input.type = "text";
-        inputConfirm.type = "text";
-        visible.innerHTML = `<img src="/img/invisible.webp" alt="invisible">`;
-    }
-    else {
-        input.type = "password";
-        inputConfirm.type = "password";
-        visible.innerHTML = `<img src="/img/visible.webp" alt="visible">`;
+    if (inputConfirm) {
+        if (input.type === 'password') {
+            input.type = "text";
+            inputConfirm.type = "text";
+            visible.innerHTML = `<img src="/img/invisible.webp" alt="invisible">`;
+        } else {
+            input.type = "password";
+            inputConfirm.type = "password";
+            visible.innerHTML = `<img src="/img/visible.webp" alt="visible">`;
 
+        }
+
+    } else {
+        
+        if (input.type === 'password') {
+            input.type = "text";
+            visible.innerHTML = `<img src="/img/invisible.webp" alt="invisible">`;
+        } else {
+            input.type = "password";
+            visible.innerHTML = `<img src="/img/visible.webp" alt="visible">`;
+            
+        }
     }
+
 }
--- public/js/signIn.js
+++ public/js/signIn.js
@@ -17,16 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let visible = document.getElementById('visible');
     let input = document.getElementById('password');
 
-    visible.addEventListener("click", function () {
-        if (input.type === 'password') {
-            input.type = "text";
-            visible.innerHTML = `<img src="/img/invisible.webp" alt="invisible">`;
-        } else {
-            input.type = 'password';
-            visible.innerHTML = `<img src="/img//visible.webp" alt="visible">`;
-        }
-
-    });
+    visible.addEventListener("click", input, null, visible);
 
     form.addEventListener('change', function (e) {
 
